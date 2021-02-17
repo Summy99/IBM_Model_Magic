7975f332b985f4579e0af78def2f62004c9a6e82
Summy99 <dylan_hensley@bloomfield.edu>
2021-02-17 15:54:51 -0500

    boss attack 1

--- Assets/Sumner's Assets/Scripts/FirstBoss.cs
+++ Assets/Sumner's Assets/Scripts/FirstBoss.cs
@@ -7,20 +7,25 @@ public class FirstBoss : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-    private BulletSourceScript bml;
+    private BulletSourceScript bmll;
+    private BulletSourceScript bmlr;
 
     [SerializeField]
     private TextAsset[] patterns;
 
     private int attack = 0;
-    public int health = 300;
+    public float health = 600;
     private bool activated = false;
+    private string direction = "right";
 
-    private bool walkFinished = false;
+    private bool walkFinished = true;
+    private bool switching = false;
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
+        bmll = gameObject.transform.Find("left").GetComponent<BulletSourceScript>();
+        bmlr = gameObject.transform.Find("right").GetComponent<BulletSourceScript>();
     }
 
     void Update()
@@ -30,16 +35,119 @@ public class FirstBoss : MonoBehaviour
             rb.velocity = -(transform.up * 10);
         }
 
-        if(gameObject.transform.position.y <= 37)
+        if(!activated && gameObject.transform.position.y <= 37)
         {
             anim.SetBool("Stopped", true);
             rb.velocity = Vector2.zero;
             activated = true;
         }
 
-        if (activated && (bml.IsEnded || walkFinished))
+        if(activated && !walkFinished)
         {
+            if(direction == "right" && transform.position.x > 12)
+            {
+                direction = "left";
+                rb.velocity = -(transform.right * 10);
+            }
 
+            else if (direction == "left" && transform.position.x < -56)
+            {
+                direction = "right";
+                rb.velocity = transform.right * 10;
+            }
         }
+
+        if(bmll.IsEnded && bmlr.IsEnded)
+        {
+            anim.SetBool("Crying", false);
+        }
+
+        if (activated && (bmll.IsEnded && bmlr.IsEnded) && walkFinished && !switching)
+        {
+            StartCoroutine("SwitchAttacks");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Letter"))
+        {
+            TakeDamage(collision.gameObject.GetComponent<ShotController>().damage);
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("PlayerShot"))
+        {
+            TakeDamage(collision.gameObject.GetComponent<ShotController>().damage);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator SwitchAttacks()
+    {
+        switching = true;
+        yield return new WaitForSeconds(1);
+        attack = Mathf.FloorToInt(Random.Range(0, 5));
+
+        switch (attack)
+        {
+            case 0:
+                StartCoroutine("Walk");
+                break;
+
+            case 1:
+                break;
+
+            case 2:
+                anim.SetBool("Crying", true);
+                bmll.xmlFile = patterns[0];
+                bmll.Initialize();
+                bmlr.xmlFile = patterns[0];
+                bmlr.Initialize();
+                break;
+
+            case 3:
+                anim.SetBool("Crying", true);
+                bmll.xmlFile = patterns[0];
+                bmll.Initialize();
+                bmlr.xmlFile = patterns[0];
+                bmlr.Initialize();
+                break;
+        }
+        
+        switching = false;
+    }
+
+    private IEnumerator Walk()
+    {
+        walkFinished = false;
+        anim.SetBool("Stopped", false);
+        
+        if(Mathf.FloorToInt(Random.Range(0, 2)) == 0)
+        {
+            direction = "right";
+            rb.velocity = transform.right * 10;
+        }
+        else
+        {
+            direction = "left";
+            rb.velocity = -(transform.right * 10);
+        }
+
+        yield return new WaitForSeconds(Random.Range(1, 6));
+
+        walkFinished = true;
+        anim.SetBool("Stopped", true);
+        rb.velocity = Vector2.zero;
     }
 }
